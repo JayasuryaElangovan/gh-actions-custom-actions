43f6b03977a7230476073eed8eb2ae00fca0bc18
Jayasurya E <jayasurya.a.e@capgemini.com>
2025-03-02 22:24:51 +0530

    Added s3 deploy custom action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,7 +1,13 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// const github = require("@actions/github");
 const exec = require("@actions/exec");
 function run() {
+  const bucket = core.getInput("bucket-name", { required: true });
+  const region = core.getInput("region", { required: true });
+  const dist = core.getInput("dist-folder", { required: true });
+  const s3uri = `s3://${bucket}/`;
+
+  exec.exec(`aws s3 sync ${dist} ${s3uri} --region ${region}`);
   core.notice("Hello from my custome js action");
 }
 run();
