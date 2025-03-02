532a727d3f252b7026d4a73b49e5e951f48d7ac0
Jayasurya E <jayasurya.a.e@capgemini.com>
2025-03-02 21:08:22 +0530

    Removed todo

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <header>
         <div id="logo-img">
-          <img src={logo} /> //TODO: Add the logo image
+          <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
       </header>
